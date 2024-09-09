7ccc54bd17b0198eb65c2e852260cc3bd362795b
vikrant-vikrant <123059176+vikrant-vikrant@users.noreply.github.com>
2024-09-09 07:38:34 +0530

    Restructuring Listings & Reviews

---
+++ routes/listing.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync");
+const ExpressError = require("../utils/ExpressError.js");
+const {listingSchema}= require("../schema.js");
+const Listing = require("../models/listing.js");
+
+const validateListing = (req,res,next)=>{
+  let {error} = listingSchema.validate(req.body);
+  if(error){
+    let errMsg = error.details.map((el)=>el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  }else{
+    next();
+  }
+};
+//Index route
+router.get("/",async(req,res)=>{
+  const allListing = await Listing.find({});
+  res.render("listings/index.ejs",{allListing});
+});
+//new route
+router.get("/new",(req,res)=>{
+  res.render("listings/new.ejs")
+});
+//show route
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id).populate("reviews");
+  if (!listing) {
+    throw new ExpressError(404, "Listing not found");
+  }
+  res.render("listings/show.ejs", { listing });
+});
+//create route
+router.post("/",validateListing,async (req,res,next)=>{
+  const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+//edit route
+//photo is not sowing up properlly after editing
+router.get("/:id/edit", async(req,res)=>{
+  let {id} = req.params;
+  const listing = await Listing.findById(id);
+  if (!listing) {
+    const { statusCode = 500, message = "Something went wrong" } = err;
+  res.status(statusCode).render("error.ejs",{message});
+  };
+  res.render("listings/edit.ejs",{listing});
+});
+//update route
+router.put("/:id",validateListing,async (req,res)=>{
+  const {id} = req.params;
+  await Listing.findByIdAndUpdate(id,{...req.body.listing});
+  res.redirect(`/listings/${id}`);
+});
+//delete route
+//unable to delete all reviews of listing from dbs
+router.delete("/:id",async(req,res)=>{
+  let {id} = req.params;
+  const deletedListing = await Listing.findByIdAndDelete(id);
+  if (!deletedListing) {
+    throw new ExpressError(404, "Listing not found");
+  };
+  res.redirect(`/listings`);
+});
+module.exports = router;
---
+++ routes/review.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router({mergeParams:true});
+const wrapAsync = require("../utils/wrapAsync");
+const ExpressError = require("../utils/ExpressError");
+const { reviewSchema } = require("../schema.js");
+const Listing = require("../models/listing");
+const Review = require("../models/review");
+
+const validateReview = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+// Post review 
+// unable to write a review 
+router.post("/", validateReview, wrapAsync(async (req, res) => {
+  const listing = await Listing.findById(req.params.id);
+  const newReview = new Review(req.body.review);
+  listing.reviews.push(newReview);
+  await newReview.save();
+  await listing.save();
+  res.redirect(`/listings/${listing._id}`);
+}));
+
+// Delete review
+// unable to delete a review 
+router.delete("/:reviewId", wrapAsync(async (req, res) => {
+  const { id, reviewId } = req.params;
+  await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+  await Review.findByIdAndDelete(reviewId);
+  res.redirect(`/listings/${id}`);
+  
+}));
+module.exports = router;
