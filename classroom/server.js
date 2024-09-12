4996b0ce666e7cc5d2dac4b2bee42afe8c155763
vikrant-vikrant <123059176+vikrant-vikrant@users.noreply.github.com>
2024-09-12 09:56:30 +0530

    use express session

--- classroom/server.js
+++ classroom/server.js
@@ -5,6 +5,8 @@ const app = express();
 const cookieParser = require("cookie-parser");//this module is use to send cookies directly,
 app.use(cookieParser("SecretCode"));
 
+const session = require("express-session");//it will save the infomation of a session in cookie form
+
 app.get("/getcookies",(req,res)=>{
   res.cookie("IronMan","i am TonyStark");//we can also add cookies mannually form consol
   res.cookie("greet","Namaste");
@@ -28,6 +30,38 @@ app.get("/verify",(req,res)=>{
   console.log(req.cookies);//it'll print unsigned cookies
   console.log(req.signedCookies);//it'll print signed cookies
   res.send("verified");
+});
+
+const sessionOptions = session({
+  secret:"mysupersecretstring",
+  resave:false,
+  saveUninitialized:true
+});
+app.use(sessionOptions);
+
+app.get("/test",(req,res)=>{
+  res.send("test successful");
+});
+
+//this is a session and it count number of request
+app.get("/reqcount",(req,res)=>{
+  if(req.session.count){
+  req.session.count++;
+  }else{
+    req.session.count = 1;
+  }
+  res.send(`You sent a request ${req.session.count} times`);
+});
+
+//storing $ using session info
+app.get("/register",(req,res)=>{
+  let {name="Anonymous"} = req.query;
+  req.session.name = name;//this'll store the data
+  res.redirect("/hello");
+});
+
+app.get("/hello",(req,res)=>{
+  res.send(`Hello ${req.session.name}`)
 })
 
 app.listen(3000,()=>{
