a00c51b4bea42c7eaa5e25a6e20e50df4e6fe869
vikrant-vikrant <123059176+vikrant-vikrant@users.noreply.github.com>
2025-03-01 07:15:31 +0530

    use Router.route for listings, reviews, and users

--- routes/listing.js
+++ routes/listing.js
@@ -4,42 +4,31 @@ const wrapAsync = require("../utils/wrapAsync");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
-//Index route
-router.get("/", listingController.index);
 
-//new route
+router
+  .route("/")
+  .get(listingController.index)
+  .post(isLoggedIn, validateListing, listingController.createListing);
+
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
-//show route
-router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  const listing = await Listing.findById(id)
-    .populate("reviews")
-    .populate("owner");
-  if (!listing) {
-    req.flash("error", "Listing you requested for does not exist");
-    res.redirect("/listings");
-  }
-  res.render("listings/show.ejs", { listing });
-});
-// router.get("/id", listingController.showListing);
-
-//create route
-router.post("/", isLoggedIn, validateListing, listingController.createListing);
-
-//edit route
-router.get("/:id/edit", isLoggedIn, listingController.renderEditForm);
-
-//update route
-router.get(
-  "/:id/edit",
-  isLoggedIn,
-  isOwner,
-  validateListing,
-  listingController.updateListing
-);
-//delete route
-//unable to delete all reviews of listing from dbs
-router.delete("/:id", isLoggedIn, isOwner, listingController.destroyListing);
-
+router
+  .route("/:id")
+  .get(async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id)
+      .populate("reviews")
+      .populate("owner");
+    if (!listing) {
+      req.flash("error", "Listing you requested for does not exist");
+      res.redirect("/listings");
+    }
+    res.render("listings/show.ejs", { listing });
+  })
+  .delete(isLoggedIn, isOwner, listingController.destroyListing);
+  
+router
+  .route("/:id/edit")
+  .get(isLoggedIn, listingController.renderEditForm)
+  .get(isLoggedIn, isOwner, validateListing, listingController.updateListing);
 module.exports = router;
--- routes/review.js
+++ routes/review.js
@@ -1,41 +1,10 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync");
-// const ExpressError = require("../utils/ExpressError.js");
-// const Review = require("../models/review");
-// const Listing = require("../models/listing");
 const { validateReview, isLoggedIn } = require("../middleware.js");
 const { createReview, destroyReview } = require("../controllers/reviews.js");
-// Post review
-// unable to write a review
-// router.post(
-//   "/",
-//   isLoggedIn,
-//   validateReview,
-//   wrapAsync(async (req, res) => {
-//     let listing = await Listing.findById(req.params.id);
-//     let newReview = new Review(req.body.review);
-//     newReview.author = req.user._id;
-//     listing.reviews.push(newReview);
-//     await newReview.save();
-//     await listing.save();
-//     req.flash("success", "New Review Created");
-//     res.redirect(`/listings/${listing._id}`);
-//   })
-// );
+
 router.post("/", isLoggedIn, validateReview, createReview);
 
-// Delete review
-// unable to delete a review
-// router.delete(
-//   "/:reviewId",
-//   wrapAsync(async (req, res) => {
-//     const { id, reviewId } = req.params;
-//     await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-//     await Review.findByIdAndDelete(reviewId);
-//     req.flash("success", "Review Deleted!");
-//     res.redirect(`/listings/${id}`);
-//   })
-// );
 router.delete("/:reviewId", wrapAsync(destroyReview));
 module.exports = router;
