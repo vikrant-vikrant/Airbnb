46f7813f447f39ae2d9e8f38f7acf1cf89b9c069
vikrant-vikrant <123059176+vikrant-vikrant@users.noreply.github.com>
2025-03-16 19:29:21 +0530

    showing coordinates on map

--- public/js/map.js
+++ public/js/map.js
@@ -4,6 +4,6 @@ let url = `https://api.tomtom.com/style/2/custom/style/${mapId}/drafts/0.json?ke
 const map = new maplibregl.Map({
   container: "map",
   style: url,
-  center: [77.209, 28.6139],
+  center: coordinates,
   zoom: 9,
 });
